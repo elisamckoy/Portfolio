5499ef31215943f832d4a6b768ada08dbe9dcf82
Beth McKoy <elisabeth.mckoy@gmail.com>
2022-07-19 00:22:10 +0100

    Style Updates
    Updating colours and icons

--- js/app.js
+++ js/app.js
@@ -2,8 +2,12 @@ $('.mode-btn').on('click', function () {
 	if ($('html').hasClass('dark-mode')) {
 		$('html').removeClass('dark-mode');
 		$('html').addClass('light-mode');
+		$('.dark-mode-btn').removeClass('d-none');
+		$('.light-mode-btn').addClass('d-none');
 	} else if ($('html').hasClass('light-mode')){
 		$('html').removeClass('light-mode');
 		$('html').addClass('dark-mode');
+		$('.dark-mode-btn').addClass('d-none');
+		$('.light-mode-btn').removeClass('d-none');
 	}
 });
