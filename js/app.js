f7829f80a2f62256bdce3c37b292b83149ab5145
Beth McKoy <elisabeth.mckoy@gmail.com>
2022-07-18 22:27:40 +0100

    Fonts and mode svgs
    Updating fonts and light mode / dark mode svgs

--- js/app.js
+++ js/app.js
@@ -1,12 +1,8 @@
-$('.light-mode-btn').on('click', function () {
+$('.mode-btn').on('click', function () {
 	if ($('html').hasClass('dark-mode')) {
 		$('html').removeClass('dark-mode');
 		$('html').addClass('light-mode');
-	}
-});
-
-$('.dark-mode-btn').on('click', function () {
-	if ($('html').hasClass('light-mode')) {
+	} else if ($('html').hasClass('light-mode')){
 		$('html').removeClass('light-mode');
 		$('html').addClass('dark-mode');
 	}
